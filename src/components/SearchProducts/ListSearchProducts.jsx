afd29e7e4a2fe09dd16d3f427d24c271bc93f015
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-01-11 22:30:49 -0300

    css list search category initialized

--- src/components/SearchProducts/ListSearchProducts.jsx
+++ src/components/SearchProducts/ListSearchProducts.jsx
@@ -5,17 +5,21 @@ const ListSearchProducts = ({ products, categories }) => {
     <div className='search-list-product'>
       {products.map((product, index) => (
         <button key={index}>
-          <p>{product.name}</p>
-          <p>{product.price}</p>
-          <p>
-            {
-              categories
-                .filter(category => category.id === product.category_id)
-                .map(category => category.name)[0] || 'Categoria não encontrada'
-            }
-          </p>
+          <div><img src={product.image_url} alt="produto" /></div>
+          <div className='produts-search'>
+            <p className='name-search'>{product.name}</p>
+            <p className='price-search'>{'R$ ' + product.price}</p>
+            <p className='search-category'>
+              {
+                categories
+                  .filter(category => category.id === product.category_id)
+                  .map(category => category.name)[0] || 'Categoria não encontrada'
+              }
+            </p>
+          </div>
         </button>
       ))}
+
     </div>
   );
 };
